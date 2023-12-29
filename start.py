00bff4db33ab95d21a6da21fc728be0b5e0c024d
Bruno Ferreira <41488641+brn83@users.noreply.github.com>
2023-12-29 12:54:41 -0300

    criação do arqui start.py

---
+++ start.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+    """Hello World Multi Línguas.
+    
+    Dependendo a língua configurada no ambiente o programa 
+    exibe a msg correspondente.
+    
+    Usage: 
+    
+    Tenha a váriavel LANG devidamente configurada ex:
+    
+        export LANG=pt_BR
+        
+    Run: 
+        python3 start.py
+        or
+        ./hello.py    
+    """
+    
+# Este programa imprime Hello World
+
+    print("Hello, World!")
