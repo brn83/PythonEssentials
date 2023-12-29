5e639bf132941b8e62d082efa2c358f384438255
Bruno Ferreira <41488641+brn83@users.noreply.github.com>
2023-12-29 13:04:15 -0300

    var msg

--- start.py
+++ start.py
@@ -18,7 +18,8 @@
 __version__ = "0.0.1"
 __author__ = "Bruno Fereira"
 __license__ ="Unlicense"
-  
+
+msg = "Hello, World!"
     
 if __name__ == "__main__":
-    print("Hello, World!")
+    print(msg)
